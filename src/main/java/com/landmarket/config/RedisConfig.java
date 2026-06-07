9efa588fddbbe3832d2fbf1946cc02c8e4b02e4e
Yarramreddy Karthik <karthikchowdary2003y@gmail.com>
2026-06-07 12:17:17 +0530

    Update RedisConfig.java

--- src/main/java/com/landmarket/config/RedisConfig.java
+++ src/main/java/com/landmarket/config/RedisConfig.java
@@ -1,26 +1,27 @@
 package com.landmarket.config;
 
-
-import java.time.Duration;
-
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
 
 @Configuration
-@EnableCaching  // <-- very important, don't miss this
+@EnableCaching
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
 public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
         RedisCacheConfiguration config = RedisCacheConfiguration
             .defaultCacheConfig()
-            .entryTtl(Duration.ofSeconds(60))   // cache expires in 60s
+            .entryTtl(Duration.ofSeconds(60))
+            .disableCachingNullValues()
             .serializeValuesWith(
                 RedisSerializationContext.SerializationPair.fromSerializer(
                     new GenericJackson2JsonRedisSerializer()
